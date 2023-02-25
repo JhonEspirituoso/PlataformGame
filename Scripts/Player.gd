698a754680340d171444152069c5da829984731f
JhonEspirituoso <jonathasrikelme.g@gmail.com>
2023-02-24 23:43:00 -0300

    Update: Addition of a new enemy in the game and system of life and damage

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,6 +4,11 @@ var move_speed = 480
 var gravity = 1200
 var jump_force = -720
 var is_grounded
+var health = 3
+var hurted = false
+var knockback_dir = 1
+var knockback_int = 500
+
 onready var raycasts = $raycasts
 
 func _physics_process(delta: float) -> void:
@@ -22,6 +27,7 @@ func _get_input():
 	
 	if move_direction !=0:
 		$Texture.scale.x = move_direction
+		knockback_dir = move_direction
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and is_grounded:
@@ -35,9 +41,36 @@ func _check_is_ground():
 
 func _set_animation():
 	var anim = "Idle"
+	
 	if !is_grounded:
 		anim = "jump"
+	
 	elif velocity.x != 0:
 		anim = "run"
+	
+	if velocity.y > 0 and !is_grounded:
+		anim = "fall"
+		
+	if hurted:
+			anim = "hit"
+	
 	if $anim.assigned_animation != anim:
 		$anim.play(anim)
+
+func knockback():
+	velocity.x = -knockback_dir * knockback_int
+	velocity = move_and_slide(velocity)
+	
+func _on_Hurtbox_body_entered(body: Node) -> void:
+	print("Funcionou")
+	health -= 1
+	hurted = true
+	knockback()
+	get_node("Hurtbox/Collision").set_deferred("disabled", true)
+	yield(get_tree().create_timer(0.5), "timeout")
+	get_node("Hurtbox/Collision").set_deferred("disabled", false)
+	hurted = false
+	if health < 1:
+		queue_free()
+		get_tree().reload_current_scene()
+	
